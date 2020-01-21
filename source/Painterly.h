7e036f5a90a430f7c8412b82ec7e9836f94bb3c9
Young hyeon Lee <50348995+Ryeoryeon@users.noreply.github.com>
2020-01-22 05:33:27 +0900

    Update Painterly.h

--- source/Painterly.h
+++ source/Painterly.h
@@ -24,13 +24,11 @@ class stroke
 public:
 	std::vector<layer> Painterly_initialize();
 	std::vector<layer> layer_list; // 나중에 접근함수 만들어서 바꿔주기
-	void paint(int T, cv::Mat & canvas, const cv::Mat & reference, std::vector<layer> & layer_list);
+	cv::Mat paint(int T, cv::Mat& canvas, const cv::Mat& reference, std::vector<layer>& layer_list);
 	int calculate_margin(int layer, int length);
 
 	int get_layersize() { return layer_size; };
 	void put_layersize(int input_lay) { layer_size = input_lay; }
-	void put_brushsize(int lay, int size) { layer_list[lay].brush_size = size; }
-	void put_fg(int lay, int fg) { layer_list[lay].f_g = fg; }
 	void push_back(int brush_size, double f_g, int grid) { layer_list.push_back(layer(brush_size, f_g, grid)); }
 
 };
